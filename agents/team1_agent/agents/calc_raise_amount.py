13b5f1c7aabf821e70d899f57e05234e44cd4d98
ShimbaF <your_email@example.com>
2025-08-15 15:47:41 +0900

    不要ファイルの削除

--- agents/team1_agent/agents/calc_raise_amount.py
+++
@@ -1,9 +0,0 @@
-from google.adk.agents import Agent
-
-calc_raise_amount = Agent(
-    name="calc_raise_amount",
-    model="gemini-2.5-flash-lite",
-    description=("Calculates the amount to raise based on the situation."),
-    instruction=("あなたはレイズ量を計算します。"
-                "あなたはanalyze_agentの結果を参考に、最適なレイズ量を計算します。")
-)
